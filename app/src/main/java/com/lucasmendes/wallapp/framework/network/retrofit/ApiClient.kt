ce96b4b20b6685834cf9b80daa29f08b4ec1174f
Mendes de Souza <lsmu@gft.com>
2023-09-12 19:21:09 -0300

    Criação do Modulo Core

---
+++ app/src/main/java/com/lucasmendes/wallapp/framework/network/retrofit/ApiClient.kt
@@ -0,0 +1,15 @@
+package com.lucasmendes.wallapp.framework.network.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiClient<T>(url: String, okHttpClient: OkHttpClient, converterFactory: GsonConverterFactory) {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(url)
+        .client(okHttpClient)
+        .addConverterFactory(converterFactory)
+        .build()
+
+    fun creatApi(apiInterface: Class<T>): T = retrofit.create(apiInterface)
+}
